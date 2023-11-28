27b277e734427a7a9450ba0f824c939a7fa7b7d2
Ahmad Purnomo <purnomo.ahmad97@gmail.com>
2023-11-28 15:08:04 +0700

    feat: add timezone

--- utils/date.ts
+++ utils/date.ts
@@ -1,5 +1,8 @@
-export const dateFormatter = (date: Date) =>
-  new Intl.DateTimeFormat('id-ID', {
+export const dateFormatter = (date: Date) => {
+  const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  return new Intl.DateTimeFormat('id-ID', {
     dateStyle: 'medium',
     timeStyle: 'long',
+    timeZone: userTimeZone,
   }).format(date);
+};
